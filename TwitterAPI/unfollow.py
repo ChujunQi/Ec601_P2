1aff1c5a1b9ecb891d861fc0482d23cf24704f8f
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-21 14:36:37 -0400

    Update unfollow.py

--- TwitterAPI/unfollow.py
+++ TwitterAPI/unfollow.py
@@ -10,18 +10,20 @@ response = requests.request("DELETE", url, headers=headers, data=payload)
 
 print(response.text)
 
-#unfollow a user specfic code
+#get user's info
 consumer = 'this is your consumer key'
 c_secret = "your consumer secret"
 token = "your access token"
 at_secret = "your access token secret"
 
+#get the user
 person = tweepy.OAuthHandle(consumer, c_secret)
 person.set_access_token(token, at_secret)
 
 api = tweepy.API(person)
 screen = "the screen_name that the user you want to check"
 
+# unfollow the user with his or her screen name
 api.destroy_friendship(screen)
 
 #unfollow a user that no following back
