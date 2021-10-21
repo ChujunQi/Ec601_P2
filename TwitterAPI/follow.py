e7d3ca254712d38081ee2735216c1c7886e3cfa9
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-21 14:27:49 -0400

    Update follow.py

--- TwitterAPI/follow.py
+++ TwitterAPI/follow.py
@@ -1,7 +1,7 @@
+# some contents about following, including follow a user, get follower's names or get a user's following, count followers.
 import os
 import tweepy
 import requests, json
-#resource: https://documenter.getpostman.com/view/9956214/T1LMiT5U#2a12978f-ebf2-4eb3-8331-c7f4bcb1e32c
 
 # follow a user
 url = "https://api.twitter.com/2/users//following"
@@ -37,14 +37,17 @@ c_secret = "your consumer secret"
 token = "your access token"
 at_secret = "your access token secret"
 
+# get the user's name
 person = tweepy.OAuthHandle(consumer, c_secret)
 person.set_access_token(token, at_secret)
 
 api = tweepy.API(person)
 screen = "the screen_name that the user you want to check"
 
+# get all followers
 all_f = tweepy.Cursor(api.followers, screen)
 
+#count how many followers
 c = 0
 for i in all_f.items():
     c += 1
