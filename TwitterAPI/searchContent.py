c1d53eeb37f83649b90e4190aecbdfc451b3661a
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-21 14:32:14 -0400

    Update searchContent.py

--- TwitterAPI/searchContent.py
+++ TwitterAPI/searchContent.py
@@ -1,3 +1,4 @@
+#search tweets that contains the content that user searches
 import os
 import tweepy
 import requests, json
@@ -10,10 +11,11 @@ without_retweet = "your key word here -filter:retweets"
 #you can change key_word to without_retweet
 yourSearch = tweepy.Cursor(api.search, q = key_word, lang = "en", since = date_since)
 
+#will print the publisher's info and location(where he or she post this)
 for i in yourSearch:
     result = [i.user.screen_name, i.user.location]
 
 '''
 you can add multiple keywords at one time. for example:
 key_word = "apple + banana" or without_retweet = "apple+banana -filter:retweets"
-
+'''
