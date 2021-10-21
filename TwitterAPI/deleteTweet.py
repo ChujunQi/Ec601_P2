0ce55c0a2197562764ebbbaf76793325a894b175
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-21 14:24:49 -0400

    Update deleteTweet.py

--- TwitterAPI/deleteTweet.py
+++ TwitterAPI/deleteTweet.py
@@ -1,9 +1,11 @@
+# this file will show how to delete a tweet
+
 import os
 import twitter
 import requests, json
 import numpy
 
-
+# get user's info
 consumer = 'this is your consumer key'
 c_secret = "your consumer secret"
 token = "your access token"
@@ -12,10 +14,11 @@ at_secret = "your access token secret"
 api = twitter.Api(consumer_key = consumer, consumer_secret = c_secret, 
                     access_token_key = token, access_token_secret = at_secret)
 
-#delete function to delete tweets
+# delete function to delete tweets
 def delete(t):
     api.DestroyStatus(t)
 
+# get tweets in a list
 myTweet = None
 with open('editedTweet.json') as json_file:
     myTweet = json.load(json_file)
