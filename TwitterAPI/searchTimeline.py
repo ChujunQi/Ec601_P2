ca0c99b3a94d43994995e5e9ad40e17d1c2a5383
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-26 16:28:36 -0400

    Update searchTimeline.py

--- TwitterAPI/searchTimeline.py
+++ TwitterAPI/searchTimeline.py
@@ -6,11 +6,12 @@ import datetime
 
 # this function will get tweets between start time and end time
 def getByTimeline(tweetList):
+    # get start time and end time 
     start = datetime.strptime('your start time', '%b %d %H:%M:%S %z %Y')
     end = datetime.strptime('your end time', '%b %d %H:%M:%S %z %Y')
 
     myTweet = []
-
+    #save all tweets between the timeline in myTweet
     for item in tweetList:
         tweet_time = datetime.strptime(item["tweet"]["created_at"], '%a %b %d %H:%M:%S %z %Y')
         if (tweet_time >= start and tweet_time <= end):
