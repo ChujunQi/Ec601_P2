1524c7ad2c1d292eaa2d56e73986c797301c6c4f
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-26 16:27:53 -0400

    Update searchTimeline.py

--- TwitterAPI/searchTimeline.py
+++ TwitterAPI/searchTimeline.py
@@ -3,30 +3,18 @@ import os, tweepy, pandas
 import twitter
 import datetime
 
-# get user's info
-consumer = 'this is your consumer key'
-c_secret = "your consumer secret"
-token = "your access token"
-at_secret = "your access token secret"
 
-api = twitter.Api(consumer_key = consumer, consumer_secret = c_secret, 
-                    access_token_key = token, access_token_secret = at_secret)
+# this function will get tweets between start time and end time
+def getByTimeline(tweetList):
+    start = datetime.strptime('your start time', '%b %d %H:%M:%S %z %Y')
+    end = datetime.strptime('your end time', '%b %d %H:%M:%S %z %Y')
 
-# get tweets
-mylist = None
-with open('editedTweet.json') as json_file:
-    mylist = json.load(json_file)
+    myTweet = []
 
-#get the timeline that the user want to search
-start = datetime.strptime('your start time', '%b %d %H:%M:%S %z %Y')
-end = datetime.strptime('your end time', '%b %d %H:%M:%S %z %Y')
+    for item in tweetList:
+        tweet_time = datetime.strptime(item["tweet"]["created_at"], '%a %b %d %H:%M:%S %z %Y')
+        if (tweet_time >= start and tweet_time <= end):
+            myTweet.append(item["tweet"]["id_str"])
 
-myTweet = []
-# use myTweet to contain tweets that the user want to see
-for item in mylist:
-    tweet_time = datetime.strptime(item["tweet"]["created_at"], '%a %b %d %H:%M:%S %z %Y')
-    if (tweet_time >= start and tweet_time <= end):
-        myTweet.append(item["tweet"]["id_str"])
-
-print(myTweet)
+    print(myTweet)
 
