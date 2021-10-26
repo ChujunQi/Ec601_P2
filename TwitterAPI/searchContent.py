c85b8bec8d39c07b23c7f5086cf09bae655f1f14
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-26 16:44:30 -0400

    Update searchContent.py

--- TwitterAPI/searchContent.py
+++ TwitterAPI/searchContent.py
@@ -4,18 +4,33 @@ import tweepy
 import requests, json
 
 
-key_word = "your key word here"
-#if you want to ignore retweet
-without_retweet = "your key word here -filter:retweets"
+# this function will search tweets by content
+def getByContent(api, keyword, no_retweet):
+    # user can input multiple keywords represent by a string, like "banana apple orange"
+    #split keywords and save in a list
+    keylist = keyword.split()
+    myWord = []
+    # if user does not want to see retweets
+    if no_retweet == True:
+        for key in keylist:
+            # get keywords one by one
+            myWord[key] = "keyword -filter:retweets"
+            search = tweepy.Cursor(api.search, q = myWord[key], lang = "en", since = date_since)
+            # get location and username of the user who tweet the content
+            for i in search:
+                result = [i.user.screen_name, i.user.location]
+                print(result)
+    # if user wants to see retweets
+    else:
+        for key in keylist:
+            # get keyword one by one
+            myWord[key] = "keyword"
+            search = tweepy.Cursor(api.search, q = myWord[key], lang = "en", since = date_since)
+            # get location and username of the user who tweet the content
+            for i in search:
+                result = [i.user.screen_name, i.user.location]
+                print(result)
 
-#you can change key_word to without_retweet
-yourSearch = tweepy.Cursor(api.search, q = key_word, lang = "en", since = date_since)
 
-#will print the publisher's info and location(where he or she post this)
-for i in yourSearch:
-    result = [i.user.screen_name, i.user.location]
 
-'''
-you can add multiple keywords at one time. for example:
-key_word = "apple + banana" or without_retweet = "apple+banana -filter:retweets"
-'''
+
