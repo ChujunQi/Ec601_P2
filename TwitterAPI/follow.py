688a9fa656a9b72f59f42231df083af4a66d5da2
ChujunQi <90354371+ChujunQi@users.noreply.github.com>
2021-10-26 15:40:38 -0400

    Update follow.py

--- TwitterAPI/follow.py
+++ TwitterAPI/follow.py
@@ -4,61 +4,57 @@ import tweepy
 import requests, json
 
 # follow a user
-url = "https://api.twitter.com/2/users//following"
+def becomeFollower(users, userid):
+    url = "https://api.twitter.com/2/users//following"
 
-payload = "{\n    \"user id that you want to follow\": \"2244994945\"\n}"
-my_header = {}
+    #here will get the user that you want to follow
+    payload = "{\n    \"userid\": \"2244994945\"\n}"
+    my_header = {}
 
-response = requests.request("POST", url, headers=my_header, data=payload)
+    response = requests.request("POST", url, headers=my_header, data=payload)
 
-print(response.text)
+    print(response.text)
 
 #get followers of a user
-get_url = "https://api.twitter.com/2/users//followers"
+def getFollower(users):
+    get_url = "https://api.twitter.com/2/users//followers"
 
-get_payload={}
-get_header = {}
+    #here two dict is empty because it will get those info from followers
+    get_payload={}
+    get_header = {}
 
-response = requests.request("GET", get_url, headers=get_header, data=get_payload)
+    response = requests.request("GET", get_url, headers=get_header, data=get_payload)
 
-print(response.text)
+    print(response.text)
 
 #get a user's following 
-u_payload={}
-u_headers = {}
+def getFollowing(users):
+    url = "https://api.twitter.com/2/users//following"
+    u_payload={}
+    u_headers = {}
 
-response = requests.request("GET", url, headers=u_headers, data=u_payload)
+    response = requests.request("GET", url, headers=u_headers, data=u_payload)
 
-print(response.text)
+    print(response.text)
 
 #count your followers
-consumer = 'this is your consumer key'
-c_secret = "your consumer secret"
-token = "your access token"
-at_secret = "your access token secret"
+def counting(myfollower):
+    c = 0
+    for i in myfollower.items():
+        c += 1
+    print(str(c))
 
-# get the user's name
-person = tweepy.OAuthHandle(consumer, c_secret)
-person.set_access_token(token, at_secret)
-
-api = tweepy.API(person)
-screen = "the screen_name that the user you want to check"
-
-# get all followers
-all_f = tweepy.Cursor(api.followers, screen)
-
-#count how many followers
-c = 0
-for i in all_f.items():
-    c += 1
-print(str(c))
 
 #get all the followers
-for x in tweepy.Cursor(api.followers, screen).items(str(c)):
-    print(x.screen)
+def getFollowerName(api, screen, count):
+    for x in tweepy.Cursor(api.followers, screen).items(str(count)):
+        print(x.screen)
+
 
 #get users that the user following
-for x in tweepy.Cursor(api.friends).items():
-    print(x.screen)
+def getFollowingName(api):
+    for x in tweepy.Cursor(api.friends).items():
+        print(x.screen)
+
 
 
